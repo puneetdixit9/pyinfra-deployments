8c22724d67c3535745c4409ee7e528948aec8874
Puneet Dixit <puneet.dixit@statusneo.com>
2023-07-28 12:26:37 +0530

    Desc: sudo su added

--- setup_server.py
+++ setup_server.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from pyinfra import local
+from pyinfra import local, config
 from pyinfra.operations import apt, files, pip, python, server
 
 from nginx_configure import update_backend_server_config
@@ -13,43 +13,62 @@ APPLICATION_PATH = os.environ.get("APPLICATION_PATH")
 SERVER_PORT = os.environ.get("SERVER_PORT")
 APPLICATION_ENV_FILE_PATH_ON_LOCAL = os.environ.get("APPLICATION_ENV_FILE_PATH_ON_LOCAL")
 APP_NAME = os.environ.get("APP_NAME")
-SERVER_PASSWORD=os.environ.get('SSH_PASSWORD')
+SERVER_PASSWORD = os.environ.get('SSH_PASSWORD')
+config.USE_SUDO_PASSWORD = SERVER_PASSWORD
 
-local.include("./tasks/common_tasks.py")
+# local.include("./tasks/common_tasks.py")
+#
 
-apt.packages(
-    name="Ensure python3.10 and python3.10-venv is Installed", packages=["python3.10", "python3.10-venv"], _sudo=True
-)
+# apt.packages(
+#     name="Ensure python3.10 and python3.10-venv is Installed", packages=["python3.10", "python3.10-venv"], _sudo=True
+# )
 
-pip.venv(
-    name="Creating Virtual Environment",
-    path=f"{APPLICATION_PATH}/venv",
-    python="python3",
-)
 
-pip.packages(
-    name="Installing requirements.txt",
-    virtualenv=f"{APPLICATION_PATH}/venv",
-    requirements=f"{APPLICATION_PATH}/requirements.txt",
-)
+commands_as_root = f"""
+su - -c'
+    add-apt-repository universe -y
+    apt update -y
+    apt upgrade -y
+    apt install -y python3.11 python3.11-venv
+'
+"""
 
 server.shell(
-    name=f"Killing the existing process on Port: {SERVER_PORT}",
-    commands=[f"kill -9 $(lsof -t -i:{SERVER_PORT})"],
-    _sudo=True,
-    _ignore_errors=True,
+    name=f"Executing sudo su operations",
+    commands=[commands_as_root],
+    _sudo=True
 )
+
+
+# pip.venv(
+#     name="Creating Virtual Environment",
+#     path=f"{APPLICATION_PATH}/venv",
+#     python="python3",
+# )
+
+# pip.packages(
+#     name="Installing requirements.txt",
+#     virtualenv=f"{APPLICATION_PATH}/venv",
+#     requirements=f"{APPLICATION_PATH}/requirements.txt",
+# )
+
+# server.shell(
+#     name=f"Killing the existing process on Port: {SERVER_PORT}",
+#     commands=[f"kill -9 $(lsof -t -i:{SERVER_PORT})"],
+#     _sudo=True,
+#     _ignore_errors=True,
+# )
 
 # files.put(name="Copy env file to the server", src=APPLICATION_ENV_FILE_PATH_ON_LOCAL, dest=f"{APPLICATION_PATH}/.env")
 
 
-server.shell(
-    name="Running Server",
-    commands=[
-        f"cd {APPLICATION_PATH} && {APPLICATION_PATH}/venv/bin/python -m flask run --host 0.0.0.0 --port {SERVER_PORT} "
-        f"> /dev/null 2>&1 &"
-    ],
-)
+# server.shell(
+#     name="Running Server",
+#     commands=[
+#         f"cd {APPLICATION_PATH} && {APPLICATION_PATH}/venv/bin/python -m flask run --host 0.0.0.0 --port {SERVER_PORT} "
+#         f"> /dev/null 2>&1 &"
+#     ],
+# )
 
 # apt.packages(name="Ensuring the nginx", packages=["nginx"], _su_user=True)
 # server.shell(
@@ -66,6 +85,6 @@ server.shell(
 #     _ignore_errors=True,
 # )
 
-python.call(name="Updating Nginx Configuration", function=update_backend_server_config)
+# python.call(name="Updating Nginx Configuration", function=update_backend_server_config)
 
-local.include("./tasks/nginx_tasks.py")
+# local.include("./tasks/nginx_tasks.py")
